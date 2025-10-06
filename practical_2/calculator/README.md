2e1df3695fda566e99454d6ed7a4787d9958ce0a
Kushal Desai <kushal.desaiofficial@gmail.com>
2025-10-06 11:10:02 +0530

    practical 2, 3

---
+++ practical_2/calculator/README.md
@@ -0,0 +1,3 @@
+# practical_2
+
+Create an application to learn Addition, subtraction, multiplication, and division for children with attractive UI.
---
+++ practical_3/android/app/src/main/kotlin/com/example/matching_images/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.matching_images
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
